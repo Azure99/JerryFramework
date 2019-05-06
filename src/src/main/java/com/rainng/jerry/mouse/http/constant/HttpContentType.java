7b02677c72c0a2096eb35a91a8b1af97cd2053fe
Azure <961523404@qq.com>
2019-05-06 13:00:06 +0800

    Add content type: octet-stream

--- src/src/main/java/com/rainng/jerry/mouse/http/constant/HttpContentType.java
+++ src/src/main/java/com/rainng/jerry/mouse/http/constant/HttpContentType.java
@@ -7,6 +7,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class HttpContentType {
+    public static final String OCTET_STREAM = "application/octet-stream";
     public static final String TEXT_PLAIN = "text/plain";
     public static final String TEXT_HTML = "text/html";
     public static final String JSON = "application/json";
