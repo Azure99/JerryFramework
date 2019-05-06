933110754f2eac5a1ad7e43d2e23a29d53dda137
Azure <961523404@qq.com>
2019-05-06 13:03:29 +0800

    Add HTTP 301 302

--- src/src/main/java/com/rainng/jerry/mouse/http/constant/HttpHeaderKey.java
+++ src/src/main/java/com/rainng/jerry/mouse/http/constant/HttpHeaderKey.java
@@ -1,10 +1,12 @@
 package com.rainng.jerry.mouse.http.constant;
 
 public class HttpHeaderKey {
-    public static final String COOKIE = "Cookie";
-    public static final String CONTENT_TYPE = "Content-Type";
     public static final String CONTENT_LENGTH = "Content-Length";
-    public static final String HOST = "Host";
-    public static final String SERVER = "Server";
+    public static final String CONTENT_TYPE = "Content-Type";
+    public static final String COOKIE = "Cookie";
     public static final String DATE = "Date";
+    public static final String HOST = "Host";
+    public static final String LOCATION = "Location";
+    public static final String SERVER = "Server";
+
 }
